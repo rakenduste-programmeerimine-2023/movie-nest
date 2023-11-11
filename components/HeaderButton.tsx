972c262578bae4cbe384cd58fb9ce6aae59d9406
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-11-11 22:22:54 +0200

    Lisasin logo, search-, log in-, ja sign-up nupud.

---
+++ components/HeaderButton.tsx
@@ -0,0 +1,32 @@
+// DeployButton.tsx
+
+import React from 'react';
+import './Buttons.css';
+import Image from 'next/image';
+
+import logo from './images/MovieNestLogo.png';
+
+export default function HeaderButton() {
+    return (
+        <div className="button-container">
+            <div className="left-buttons">
+          <Image src={logo} alt="Logo" width={120} height={100} />
+            <button className="search-button">
+                <span role="img" aria-label="luup">
+                    🔍
+                </span> 
+                Search
+            </button>
+            </div>
+
+            <div className="right-buttons">
+            <button className="log-in-button">
+                Log in
+            </button>
+            <button className="sign-up-button">
+                Sign up
+            </button>
+            </div>
+        </div>
+    );
+}
