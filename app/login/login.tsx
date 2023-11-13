1f561e8e8c3f8b2df1d15220182b43cc8ae5c7e6
Johanna Erikson <johanna5@tlu.ee>
2023-11-13 20:53:58 +0200

    Added header to login form

---
+++ app/login/login.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Buttons.css";
+import Image from "next/image";
+import logo from "./images/MovieNestLogo.png";
+
+export default function HeaderSignupLogin() {
+  return (
+    <div className="button-container">
+      <div className="left-buttons">
+        <Image src={logo} alt="Logo" width={130} height={100} />
+        <button className="search-button">
+          <span role="img" aria-label="luup">
+            🔍
+          </span>
+          Search
+        </button>
+      </div>
+      <div className="right-buttons">
+        <button className="home-button">Home</button>
+        <button className="contact-button">Contact</button>
+      </div>
+    </div>
+  );
+}
