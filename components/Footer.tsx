e68dbb79aa05b0a59ce3c59fe42b6e4eca14f250
Merilyn Litvinov <merilynl@users.noreply.github.com>
2023-11-13 15:33:14 +0200

    feat/issue-22/add footer home btn

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,6 +7,9 @@ export default function Header() {
   return (
     <div className="bottom-button-container, button-container">
       <Image src={logo} alt="Logo" width={130} height={100} />
+      <div className="right-buttons">
+        <button className="home-button">Home</button>
+      </div>
     </div>
   );
 }
