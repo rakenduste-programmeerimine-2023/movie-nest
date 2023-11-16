4c6e254aa7a111de439c7f1f5366229ef0b464cb
Johanna Erikson <johanna5@tlu.ee>
2023-11-16 20:14:54 +0200

    feat: add password input
    closes #3

--- components/login.tsx
+++ components/login.tsx
@@ -9,6 +9,11 @@ export default function Login() {
           <label htmlFor="username">Username:</label>
           <input type="text" id="username" placeholder="Enter your username" />
         </div>
+
+        <div className="input-container">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" placeholder="Enter your password" />
+        </div>
       </div>
     </div>
   );
