5c2614a1cfd12aeb38840acb2aa0168474803383
Merilyn Hunt <merilynl@tlu.ee>
2023-11-18 16:43:04 +0200

    feat: change movie nest logo

--- components/HeaderButton.tsx
+++ components/HeaderButton.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./Buttons.css";
 import Image from "next/image";
 import logo from "./images/MovieNestLogo.png";
-import Link from 'next/link'; 
+import Link from "next/link";
 
 export default function HeaderButton() {
   return (
     <div className="button-container">
       <div className="left-buttons">
-        <Image src={logo} alt="Logo" width={130} height={100} />
+        <Image src={logo} alt="Logo" className="logo-image" />
         <button className="search-button">
           <span role="img" aria-label="luup">
             🔍
