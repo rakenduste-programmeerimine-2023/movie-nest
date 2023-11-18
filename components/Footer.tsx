7e0f5fe14c4cbd6d3cc319c674fcf9a723743942
Merilyn Hunt <merilynl@tlu.ee>
2023-11-18 16:36:07 +0200

    feat: add tmdb api logo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./Buttons.css";
 import Image from "next/image";
 import logo from "./images/MovieNestLogo.png";
+import api_logo from "./images/api-logo.svg";
 
 export default function Header() {
   return (
     <div className="bottom-button-container, button-container">
-      <Image src={logo} alt="Logo" width={130} height={100} />
+      <Image src={logo} alt="Logo" className="logo-image" />
+      <div>
+        <Image src={api_logo} alt="Logo" width={130} />
+      </div>
       <div className="right-buttons">
         <button className="home-button">Home</button>
         <button className="contact-button">Contact</button>
