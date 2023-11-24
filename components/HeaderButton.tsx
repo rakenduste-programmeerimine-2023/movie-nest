d64afc6cb86273158eb5d27b989d0b0b0198affe
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-11-24 17:03:23 +0200

    feat: connected with supabase, solved log in button issue

--- components/HeaderButton.tsx
+++ components/HeaderButton.tsx
@@ -3,6 +3,7 @@ import "./Buttons.css";
 import Image from "next/image";
 import logo from "./images/MovieNestLogo.png";
 import Link from "next/link";
+import AuthButton from "./AuthButton";
 
 export default function HeaderButton() {
   return (
@@ -20,9 +21,7 @@ export default function HeaderButton() {
         <Link href="/contact">
           <button className="contact-button">Contact</button>
         </Link>
-        <Link href="/login">
-          <span className="log-in-button">Log in</span>
-        </Link>
+        <AuthButton />
       </div>
     </div>
   );
