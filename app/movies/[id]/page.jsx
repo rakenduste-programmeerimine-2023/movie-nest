8fa2b33f471cbd9d94c2e528c688d3e6818ddd4b
Merilyn Hunt <merilynl@tlu.ee>
2023-11-24 14:58:39 +0200

    feat: add favorite watched buttons to detail view

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -15,12 +15,31 @@ async function MovieDetailsPage({ params }) {
           <HeaderButton />
         </div>
       </nav>
-      <div className="flex flex-col w-screen items-center">
-        <div className="animate-in text-left text-2xl">
-          <h1 className="pb-10">{movieDetails.title}</h1>
+      <div className="flex flex-col w-fit items-center">
+        <div className="animate-in flex flex-row gap-8 items-center pb-10">
+          <h1 className="text-2xl mr-10 font-bold">{movieDetails.title}</h1>
+          <button className="mt-2 bg-gray-600 hover:bg-gray-400 text-white font-bold py-1 px-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
+          </button>
+          <button className="mt-2 bg-gray-600 hover:bg-gray-400 text-white font-bold py-2 px-4">
+            Add to watched
+          </button>
         </div>
         <div className="flex flex-row justify-end gap-10">
-          <div className="">
+          <div>
             <img src={IMAGE_BASE_URL + movieDetails.backdrop_path} />
           </div>
           <div className="animate-in w-2/4 flex flex-col gap-10">
