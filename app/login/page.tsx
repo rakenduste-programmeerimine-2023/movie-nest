2280ace9921ddbf4fb0515c6321ae085eaa58bd9
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-11-24 19:53:17 +0200

    feat: connected to supabase and solved log in buttons issues

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { headers, cookies } from 'next/headers'
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
+import '../../components/Buttons.css';
 
 export default function Login({
   searchParams,
@@ -56,7 +57,7 @@ export default function Login({
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
         href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center back-button text-sm"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -99,7 +100,7 @@ export default function Login({
           required
         />
         <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
-          Sign In
+          Log In
         </button>
         <button
           formAction={signUp}
--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <HeaderButton />
-          {isSupabaseConnected && <AuthButton />}
+          {isSupabaseConnected}
         </div>
       </nav>
 
