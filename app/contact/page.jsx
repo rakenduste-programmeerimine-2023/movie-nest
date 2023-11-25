e0dcd629b91443417c435b110cf8f837a0c65a6a
Merilyn Hunt <merilynl@tlu.ee>
2023-11-25 20:02:27 +0200

    feat: contact page add name input

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,9 @@
 import HeaderButton from "@/components/HeaderButton";
 import Footer from "@/components/Footer";
 
-export default async function ContactPage() {
+import ContactForm from "@/components/ContactForm";
+
+export default function Home() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
@@ -9,16 +11,9 @@ export default async function ContactPage() {
           <HeaderButton />
         </div>
       </nav>
-
-      <div className="animate-in flex-1 w-1/3 flex flex-col gap-10 items-start">
-        <h1 className="text-xl">Contact us</h1>
-        <div className="flex flex-col gap-10 items-start">
-          <h4>Name</h4>
-          <h4>Email</h4>
-          <h4>Message</h4>
-        </div>
+      <div className="flex-1 w-full flex flex-col items-center">
+        <ContactForm />
       </div>
-
       <footer className="w-full border-t border-t-foreground/10">
         <div className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
---
+++ components/ContactForm.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+const ContactForm = () => {
+  return (
+    <div className="page create w-1/2">
+      <h1 className="text-xl self-start pb-8">Contact us</h1>
+      <form className="flex flex-col gap-4">
+        <label htmlFor="name">Name:</label>
+        <input
+          className="text-black border border-gray-600 rounded"
+          type="text"
+          id="name"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
