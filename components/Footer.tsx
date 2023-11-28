99ab8fed90fd5181398a4ea843c66d70ce87f78a
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-11-28 20:17:05 +0200

    feat: added functionality to logo and home button to go back to home page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,16 +3,21 @@ import "./Buttons.css";
 import Image from "next/image";
 import logo from "./images/MovieNestLogo.png";
 import api_logo from "./images/api-logo.svg";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="bottom-button-container, button-container">
-      <Image src={logo} alt="Logo" className="logo-image" />
+      <Link href="http://localhost:3000/">
+        <Image src={logo} alt="Logo" className="logo-image" />
+        </Link>
       <div>
         <Image src={api_logo} alt="Logo" width={130} />
       </div>
       <div className="right-buttons">
+        <Link href="http://localhost:3000/">
         <button className="home-button">Home</button>
+        </Link>
         <button className="contact-button">Contact</button>
       </div>
     </div>
