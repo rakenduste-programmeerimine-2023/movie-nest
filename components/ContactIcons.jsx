09a375abbd1c5bf7003b584202e1a89ba564122e
Merilyn Hunt <merilynl@tlu.ee>
2023-11-28 23:13:16 +0200

    fix: fix error in contact icons round attribute

--- components/ContactIcons.jsx
+++ components/ContactIcons.jsx
@@ -17,19 +17,19 @@ const ContactIcons = () => {
   return (
     <div className="flex flex-row gap-10">
       <FacebookShareButton url={"http://localhost:3000"}>
-        <FacebookIcon size={64} square borderRadius={5} />
+        <FacebookIcon size={64} round={false} borderRadius={5} />
       </FacebookShareButton>
       <TwitterShareButton url={"http://localhost:3000"}>
-        <TwitterIcon size={64} square borderRadius={5} />
+        <TwitterIcon size={64} round={false} borderRadius={5} />
       </TwitterShareButton>
       <RedditShareButton url={"http://localhost:3000"}>
-        <RedditIcon size={64} square borderRadius={5} />
+        <RedditIcon size={64} round={false} borderRadius={5} />
       </RedditShareButton>
       <EmailShareButton url={"http://localhost:3000"}>
-        <EmailIcon size={64} square borderRadius={5} />
+        <EmailIcon size={64} round={false} borderRadius={5} />
       </EmailShareButton>
       <LinkedinShareButton url={"http://localhost:3000"}>
-        <LinkedinIcon size={64} square borderRadius={5} />
+        <LinkedinIcon size={64} round={false} borderRadius={5} />
       </LinkedinShareButton>
     </div>
   );
