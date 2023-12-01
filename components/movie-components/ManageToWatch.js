1c886d55a8a3e0bf48ff67fa0094e51ec6b37ba2
Merilyn Hunt <merilynl@tlu.ee>
2023-12-01 23:07:30 +0200

    feat: make and connect to watch button

--- components/movie-components/ManageToWatch.js
+++ components/movie-components/ManageToWatch.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 //import supabase from "../../app/config/supabaseClient";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-const ManageFavorite = ({ movieid, user }) => {
+const ManageToWatch = ({ movieid, user }) => {
   const [errorMessage, setErrorMessage] = useState("");
   const [actionMessage, setActionMessage] = useState("");
 
@@ -14,7 +14,7 @@ const ManageFavorite = ({ movieid, user }) => {
     const supabase = createClientComponentClient();
 
     const { data, select_error } = await supabase
-      .from("Favorite")
+      .from("WatchLater")
       .select("id, user_id, movie_id");
 
     if (select_error) {
@@ -30,27 +30,27 @@ const ManageFavorite = ({ movieid, user }) => {
 
     if (!existsInArray) {
       const { error } = await supabase
-        .from("Favorite")
+        .from("WatchLater")
         .insert([{ movie_id: movieid, user_id: user.id }], {
           returning: "minimal",
         });
       if (error) {
         setErrorMessage(error.message);
       } else {
-        setActionMessage("Added to favorites");
+        setActionMessage("Added to watch later");
         console.log("Entry added to database");
       }
     } else {
       console.log("Entry exists in database");
       const { data: deleteData, error: deleteError } = await supabase
-        .from("Favorite")
+        .from("WatchLater")
         .delete()
         .eq("id", existsInArray.id);
 
       if (deleteError) {
         setErrorMessage(deleteError.message);
       } else {
-        setActionMessage("Removed from favorites");
+        setActionMessage("Removed from watched");
         console.log("Entry deleted successfully");
       }
     }
@@ -60,31 +60,20 @@ const ManageFavorite = ({ movieid, user }) => {
     <div className="relative">
       <button
         onClick={submitHandler}
-        className="font-bold py-1 px-2 mr-6 border-solid border border-gray-800 rounded group transition-colors self-center flex-shrink-0"
+        className="py-1 px-2 hover:bg-gray-300 border-solid border border-gray-800 rounded self-center flex-shrink-0"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 group-hover:fill-white"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-          />
-        </svg>
+        Watch Later
       </button>
       {actionMessage && (
-        <div className="absolute bottom-0 left-0 right-0 top-11 text-xs w-2/3">
+        <div className="absolute bottom-0 left-0 right-0 top-11 text-xs">
           <p className="">{actionMessage}</p>
         </div>
       )}
-      <div>{errorMessage && <p>{errorMessage}</p>}</div>
+      <div className="absolute bottom-0 left-0 right-0 top-11 text-xs w-2/3">
+        {errorMessage && <p>{errorMessage}</p>}
+      </div>
     </div>
   );
 };
 
-export default ManageFavorite;
+export default ManageToWatch;
