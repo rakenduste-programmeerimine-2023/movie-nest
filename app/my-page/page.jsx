449f9abcf86b56a7ee3ae2ccc309cca6574137eb
Merilyn Hunt <merilynl@tlu.ee>
2023-12-02 01:39:24 +0200

    feat: my page show watch later results

--- app/my-page/page.jsx
+++ app/my-page/page.jsx
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation";
 import HeaderButton from "@/components/HeaderButton";
 import AuthButton from "@/components/AuthButton";
 import Footer from "@/components/Footer";
+import { getMovieDetails } from "../movie-requests/requests";
+import { Carousel } from "@/components/movie-components/Carousel";
 
 export default async function Account() {
   const cookieStore = cookies();
@@ -21,6 +23,26 @@ export default async function Account() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const userid = user.id.toString();
+  var errorMessage = "";
+  const WatchLaterMovieDetailsArray = [];
+
+  const { data, select_error } = await supabase
+    .from("WatchLater")
+    .select("id, user_id, movie_id")
+    .eq("user_id", userid);
+
+  if (select_error) {
+    errorMessage = select_error.message;
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    const movieDetails = await getMovieDetails(data[i].movie_id);
+    WatchLaterMovieDetailsArray.push(movieDetails);
+  }
+
+  console.log(WatchLaterMovieDetailsArray);
+
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
@@ -30,8 +52,13 @@ export default async function Account() {
         </div>
       </nav>
 
-      <div className="animate-in flex-1 w-full max-w-7xl flex flex-col gap-20 items-start text-2xl">
+      <div className="animate-in flex-1 w-full max-w-7xl flex flex-col gap-10 items-center text-2xl">
         <h1>{user.email}'s page</h1>
+        <div className="flex flex-col self-center">
+          <h1 className="ml-10 mb-4">Watch Later</h1>
+          <Carousel movies={WatchLaterMovieDetailsArray}></Carousel>
+        </div>
+        {errorMessage && <p>{errorMessage}</p>}
       </div>
 
       <footer className="w-full border-t border-t-foreground/10">
